a7044d0447b82641b46899cc4c654a912b86f46b
Bhasker23 <shrimalibhaskar2398@gmail.com>
2023-09-01 00:37:15 +0530

    VidyutTech Application

---
+++ VidyutTech/src/main/java/com/VidyutTech/VidyutTechApplication.java
@@ -0,0 +1,13 @@
+package com.VidyutTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidyutTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidyutTechApplication.class, args);
+	}
+
+}
---
+++ VidyutTech/src/test/java/com/VidyutTech/VidyutTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VidyutTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidyutTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
